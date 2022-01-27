3a851f396ff19d01bdef4f7a64ac86b5ec61f3a1
llarnold005 <74628922+llarnold005@users.noreply.github.com>
2022-01-26 23:19:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from cicd_project.cicd_project_stack import MyPipelineStack
 
 app = cdk.App()
 MyPipelineStack(app, "MyPipelineStack", 
-    env=cdk.Environment(account="683397703604", region="us-east-2")
+    env=cdk.Environment(account="465444432665", region="us-east-2")
 )
 
 app.synth()

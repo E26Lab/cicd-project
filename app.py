0c72db3681fc7e3b0dc255e401f7100ee0c6b77c
Louis  Arnold <louisarnold@Louiss-MacBook-Pro.local>
2022-02-08 02:34:01 -0600

    updated pipeline

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-import aws_cdk as cdk
+from distutils import core
+import os
+from aws_cdk import core
 from cicd_project.cicd_project_stack import MyPipelineStack
 
-app = cdk.App()
+app = core.App()
 MyPipelineStack(app, "MyPipelineStack", 
-    env=cdk.Environment(account="465444432665", region="us-east-1")
+    env={'account' : '465444432665', 'region' : 'us-east-1'}
 )
 
 app.synth()
--- cicd_project/cicd_project_stack.py
+++ cicd_project/cicd_project_stack.py
@@ -1,27 +1,46 @@
 import aws_cdk as cdk
-from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
+from aws_cdk.aws_codepipeline import Pipeline, Artifact
 from aws_cdk.aws_codepipeline_actions import GitHubSourceActionProps as codepipeline_actions
 import aws_cdk.aws_secretsmanager as secretsmanager
+import aws_cdk.aws_codecommit as codecommit
 from constructs import Construct
+from aws_cdk import pipelines
+
 
 class MyPipelineStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         
-        pipeline =  CodePipeline(self, "Pipeline") 
-        source_output = CodePipeline.Artifact()
-        source_action = codepipeline_actions.GitHubSourceAction(
+        source_artifact = cdk.aws_codepipeline.Artifact()
+        cloud_assembly_artifact = cdk.aws_codepipeline.Artifact()
+        
+        pipeline = Pipeline(self, "Pipeline",
+        cloud_assembly_artifact=cloud_assembly_artifact,
+        pipeline_name="E26Pipeline",
+        source_action = codepipeline_actions(
             action_name="GitHub_Source",
             owner="E26Lab",
             repo="cicd-project",
-            oauth_token=secretsmanager.SecretValue.secrets_manager("github-token"),
-            output=source_output,
+            oauth_token=cdk.SecretValue.secrets_manager("github-token"),
+            output=source_artifact,
             branch="main"
-        )
-        pipeline.add_stage(
-            stage_name="Source",
-            actions=[source_action]
-        )
-                       
+        ), 
+        synth_action=pipelines.SimpleSynthAction(
+                source_artifact=source_artifact,
+                cloud_assembly_artifact=cloud_assembly_artifact,
+                install_command='npm install -g aws-cdk && pip install -r requirements.txt',
+                synth_command= 'cdk synth'))
+
+
+
+
+
+
+
+
+
+        
+      
+    
                     
